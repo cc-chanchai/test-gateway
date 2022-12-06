e701ccff11acffdca1fb2f23d22cd17d8235d1ed
chaichannnnn <chanchai9797@hotmail.com>
2022-12-06 21:43:50 +0700

    create

---
+++ gateway-api-service/src/test/java/com/microservices/gatewayapiservice/GatewayApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.gatewayapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
